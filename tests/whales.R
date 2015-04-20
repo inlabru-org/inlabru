ba4c645b743474933206945637d50800c82a347e
Fabian E. Bachl <feb24@bath.ac.uk>
2015-04-20 11:00:58 +0100

    Merge branch 'package' of https://github.com/david-borchers/essmod into package

--- tests/whales.R
+++ tests/whales.R
@@ -174,7 +174,8 @@ data(whales)
                                     range = c(0, Inf),
                                     hyper = list(beta=list(initial=0,
                                                  param=c(0,0.1))))
-                                + f(spde, model=spde.mdl)),
+                                + f(spde, model=spde.mdl)
+                                ),
                      covariates =
                        list(sst1 = sstFun1,
                             sst2 = sstFun2,
@@ -185,7 +186,111 @@ data(whales)
                      prior.variance.nominal = 10,
                      theta.prior.prec = 0.01,
                      extraconstr.int =
-                       list(A=matrix(sst.mask, 1, length(sst.mask)), e=0)))
+                       list(A=matrix(sst.mask, 1, length(sst.mask)), e=0)),
+                     predict = list(
+                     comb1 = list(vars = c("Intercept","spde"),
+                     loc=data.frame(lon=whales$mesh$loc[,1],
+                                    lat=whales$mesh$loc[,2],
+                                    year=rep(2004, each=whales$mesh$n))),
+                     comb2 = list(vars = c("Intercept","spde",
+                                  "clon", "clat",
+                                  "sst1", "sst2", "sst3"),
+                     loc=data.frame(lon=rep(whales$mesh$loc[,1], times=2),
+                                    lat=rep(whales$mesh$loc[,2], times=2),
+                     year=rep(c(2004, 2006), each=whales$mesh$n)))
+                     )
+                     )
+
+  wpp4 <- pproc_lgcp(whales,
+                     int.points = whales$ips.yearly,
+                     formula = (y ~ -1
+                                + Intercept
+                                + clon
+                                + clat
+                                + sst1
+                                + sst2
+                                + sst3
+                                + f(distance, model="clinear",
+                                    range = c(0, Inf),
+                                    hyper = list(beta=list(initial=0,
+                                                 param=c(0,0.1))))
+                                ),
+                     covariates =
+                       list(sst1 = sstFun1,
+                            sst2 = sstFun2,
+                            sst3 = sstFun3,
+                            clon = clonFun,
+                            clat = clatFun),
+                     predict = list(
+                     comb2 = list(vars = c("Intercept","spde",
+                                  "clon", "clat",
+                                  "sst1", "sst2", "sst3"),
+                     loc=data.frame(lon=rep(whales$mesh$loc[,1], times=2),
+                                    lat=rep(whales$mesh$loc[,2], times=2),
+                     year=rep(c(2004, 2006), each=whales$mesh$n)))
+                     )
+                     )
+
+idx.comb1 <- inla.stack.index(wpp3$stack, "comb1")$data
+idx.comb2.2004 <- inla.stack.index(wpp3$stack, "comb2")$data[(
+    seq_len(whales$mesh$n))]
+idx.comb2.2006 <- inla.stack.index(wpp3$stack, "comb2")$data[(
+    whales$mesh$n + seq_len(whales$mesh$n))]
+
+## Basic intensity estimate:
+##   200/(321218*12)/12*2 [whales / km^2 / year] = 8.65e-6
+## log(...) = -11.7
+## log10(...) = -5.06
+
+contour(inla.mesh.project(
+    whales$mesh,
+    field=wpp3$INLA$result$summary.random$spde$mean))
+
+contour(inla.mesh.project(
+    whales$mesh,
+    field=wpp3$INLA$result$summary.linear.predictor$mean[idx.comb1]))
+
+contour(inla.mesh.project(
+    whales$mesh,
+    field=wpp3$INLA$result$summary.linear.predictor$mean[idx.comb2.2004]),
+        nlevels=50)
+
+c0 <- sst.mask * diag(inla.mesh.fem(whales$mesh)$c0)
+c(200/(321218*12)/12*2,
+  sum(c0 * exp(wpp3$INLA$result$summary.linear.predictor$"0.025quant"[idx.comb2.2004])) / sum(c0))
+
+
+contour(inla.mesh.project(
+    whales$mesh,
+    field=wpp3$INLA$result$summary.linear.predictor$sd[idx.comb1]))
+
+contour(inla.mesh.project(
+    whales$mesh,
+    field=wpp3$INLA$result$summary.linear.predictor$sd[idx.comb2.2004]),
+        nlevels=20)
+
+contour(inla.mesh.project(
+    whales$mesh,
+    field=exp(wpp3$INLA$result$summary.linear.predictor$"0.025quant"[idx.comb2.2004])),
+        nlevels=20)
+contour(inla.mesh.project(
+    whales$mesh,
+    field=exp(wpp3$INLA$result$summary.linear.predictor$"0.975quant"[idx.comb2.2004])),
+        nlevels=20)
+
+contour(inla.mesh.project(
+    whales$mesh,
+    field=wpp3$INLA$result$summary.linear.predictor$sd[idx.comb2.2004]),
+        nlevels=50)
+
+plot(wpp3$INLA$result$summary.linear.predictor$mean[idx.comb2.2004],
+     wpp3$INLA$result$summary.linear.predictor$sd[idx.comb2.2004])
+
+## c.v. for lognormal:  sqrt(exp(sigma^2)-1)
+plot(exp(wpp3$INLA$result$summary.linear.predictor$mean[idx.comb2.2004]),
+     sqrt(exp(2*wpp3$INLA$result$summary.linear.predictor$sd[idx.comb2.2004]) - 1), log="xy")
+
+plot.dfun_halfnormal(wpp3)
 
 #
 # Latitude and Longitude as covariate data structures
