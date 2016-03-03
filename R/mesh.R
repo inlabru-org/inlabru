10ffffaf28281481fcfae67cb390b04a5bef0012
Fabian E. Bachl <feb24@bath.ac.uk>
2016-03-03 10:02:41 +0000

    Add experimental method to plot mesh using ggplot2

--- R/mesh.R
+++ R/mesh.R
@@ -1,3 +1,58 @@
+#' Plot an inla.mesh using ggplot
+#'
+#' @aliases ggp.mesh
+#' @export
+#' @param mesh an inla.mesh object
+#' @author Fabian E. Bachl <\email{f.e.bachl@@bath.ac.uk}>
+
+ggp.mesh = function(mesh, col = NULL, nx = 400, add = NULL) {
+  
+  xlim = range(mesh$loc[,1])
+  ylim = range(mesh$loc[,2])
+  aspect = diff(ylim)/diff(xlim)
+  ny = round(nx * aspect)
+  proj <- inla.mesh.projector(mesh, dims = c(nx,ny))
+  x = seq(xlim[1], xlim[2], length.out = nx)
+  y = seq(ylim[1], ylim[2], length.out = ny)
+  grid = expand.grid(x=x,y=y)
+  
+  
+  if ( !is.null(col) ) {
+    mcol = rgb(0,0,0,0.1)
+    loc = data.frame(cbind(grid$x,grid$y))
+    col = inla.spde.make.A(mesh, loc = as.matrix(loc)) %*% as.vector(col)
+    msk = is.inside(mesh,as.matrix(loc))
+    # col[msk] = NA
+    df = data.frame(grid, col=as.vector(col), alpha = msk)
+    gg = ggplot(df, aes(x=x,y=y) )
+    gg = gg + geom_raster(aes(fill = col, alpha = alpha), hjust=0.5, vjust=0.5, interpolate = TRUE)
+    gg = gg + scale_alpha_discrete(guide = 'none')
+    gg = gg + scale_fill_gradientn(colours = topo.colors(100) ) + theme(legend.title=element_blank())
+    
+  } else {
+    mcol = rgb(0,0,0,0.8)
+    df = data.frame(grid)
+    if (!is.null(add)) { gg = add } else { gg = ggplot(df, aes(x=x,y=y) )}
+  }
+  
+  # Plot mesh lines
+  gg = gg + geom_segment(data = data.frame(a=mesh$loc[mesh$graph$tv[,1],c(1,2)],b=mesh$loc[mesh$graph$tv[,2],c(1,2)]), 
+                         aes(x=a.1,y=a.2,xend=b.1,yend=b.2), color = mcol)
+  
+  gg = gg + geom_segment(data = data.frame(a=mesh$loc[mesh$graph$tv[,2],c(1,2)],b=mesh$loc[mesh$graph$tv[,3],c(1,2)]),
+                         aes(x=a.1,y=a.2,xend=b.1,yend=b.2), color = mcol)
+  
+  gg = gg + geom_segment(data = data.frame(a=mesh$loc[mesh$graph$tv[,1],c(1,2)],b=mesh$loc[mesh$graph$tv[,3],c(1,2)]),
+                         aes(x=a.1,y=a.2,xend=b.1,yend=b.2), color = mcol)
+  
+  gg = gg + coord_fixed()
+
+  return(gg)
+}
+
+
+
+
 #' Query if a point is inside the mesh boundary
 #'
 #'
