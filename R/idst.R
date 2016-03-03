dd51099e03fa85b6bb50e0962dbd9c80a9bbd5cd
Fabian E. Bachl <feb24@bath.ac.uk>
2016-03-03 09:46:52 +0000

    Add a wrapper for INLA

---
+++ R/idst.R
@@ -0,0 +1,37 @@
+#' iDistance wrapper for INLA
+#' 
+#'
+#' @aliases idst
+#' @export
+#' @param data A \link{dsdata} object
+#' @param model A \link{model} object
+#' @param ips A data.frame of integration points
+#' @param stack A stack configuration (Not implemented yet, use NULL)
+#' @param n Number of \link{inla} iterations
+#' @param ... Arguments passed on to \link{inla}
+#' @return A \link{inla} object
+
+
+idst = function(data, model, ips = NULL, stack = NULL, n = 1, ...){
+  
+  if ( is.null(stack) ) {
+    det.stack <- detection.stack(data, model = model)
+    int.stack <- integration.stack(data, scheme = ips, model = model)
+    stack <- inla.stack(det.stack, int.stack)  
+  }
+
+  for ( k in 1:n ) {
+    result <- inla(formula = model$formula, 
+                   family = "poisson",
+                   data = c(inla.stack.data(stack), list.data(model)),
+                   control.predictor = list( A = inla.stack.A(stack), compute = TRUE),
+                   E = inla.stack.data(stack)$e,
+                   ...)
+    if ( k > 1) {
+      update.model(model, result)
+      det.stack <- detection.stack(data, model = model)
+      int.stack <- integration.stack(data, scheme = ips, model = model)
+      stack <- inla.stack(det.stack, int.stack)  
+    }
+  }
+}
