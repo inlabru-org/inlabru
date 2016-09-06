4e331bd1901f990186c06ee716c839e4748e9226
Fabian E. Bachl <bachlfab@gmail.com>
2016-09-06 18:49:25 +0100

    Add conversion of dsdata to Spatial* objects

--- R/dsdata.R
+++ R/dsdata.R
@@ -914,3 +914,37 @@ segment.id = function(effort) { as.numeric(gsub("^.*\\.","", as.character(effort
 #' @author Fabian E. Bachl <\email{f.e.bachl@@bath.ac.uk}>
 
 detection.id = function(effort) { as.numeric(gsub("^.*\\.","", as.character(effort$det))) }
+
+
+#' Convert dsdata into Spatial* objects
+#'  
+#' @aliases as.spatial.dsdata
+#' @export
+#' @param dsdata dsdata object
+#' @param cnames Column names of the coordinates, e.g. c("lon","lat")
+#' @param crs Coordinate reference system, e.g. CRS("+proj=longlat")
+#' @return list with spatial points, spatial lines and a mesh
+#' @author Fabian E. Bachl <\email{f.e.bachl@@bath.ac.uk}>
+
+as.spatial.dsdata = function(dset, cnames, crs){
+  
+  # Extract detections and convert to SpatialPointsDataFrame
+  points = SpatialPointsDataFrame(coords = as.data.frame(detdata(dset))[,cnames], 
+                                  data = as.data.frame(detdata(dset))[,setdiff(names(detdata(dset)), cnames)],
+                                  proj4string = crs)
+  
+  # Extract effort and convert to SpatialLinesDataFrame
+  sp = as.data.frame(segdata(dset)[,paste0("start.",cnames)])
+  ep = as.data.frame(segdata(dset)[,paste0("end.",cnames)])
+  colnames(sp) = cnames
+  colnames(ep) = cnames
+  lilist = lapply(1:nrow(sp), function(k) { Lines(list(Line(rbind(sp[k,],ep[k,]))), ID = k) } )
+  
+  spl = SpatialLines(lilist, proj4string = crs)
+  df = as.data.frame(segdata(dset))[,setdiff(names(segdata(dset)), c(paste0("start.",cnames),paste0("end.",cnames)))]
+  rownames(df) = 1:nrow(df)
+  samplers = SpatialLinesDataFrame(spl, data = df)
+  
+  # Return detections ("points") and effort ("samplers")
+  list(points = points, samplers = samplers, mesh = dset$mesh)
+}
