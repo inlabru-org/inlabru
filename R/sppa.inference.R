2dc6808c4148742996a3cfc4fd5ac3a9041ad97a
Fabian E. Bachl <bachlfab@gmail.com>
2016-10-15 08:27:58 +0100

    Fix setting effect name if multiple random effects are present

--- R/sppa.inference.R
+++ R/sppa.inference.R
@@ -275,7 +275,7 @@ as.model.formula = function(fml) {
       mesh.coords[[ge$term]] = ge$term
       # mesh.map[[ge$term]] = function(x) { x[,ge$term,drop=FALSE]}
       inla.models[[ge$term]] = ge$model
-      effects[[gidx]] = ge$term
+      effects[[k]] = ge$term
       # Replace function name by INLA f function
       lb = gsub("g\\(","f(",lb)
       # Remove extra mesh argument
