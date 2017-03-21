064b41cb5320f7420a1370cbdbe9ba86f5f83256
Finn Lindgren <finn.lindgren@gmail.com>
2017-03-21 09:14:07 +0000

    Add Matern covaraince credible bands calculator

--- R/spde.R
+++ R/spde.R
@@ -1,3 +1,135 @@
+#' @title Matern correlation or covariance function approximate credible bands.
+#' 
+#' @description Evaluate the covariance function for an inla.spde
+#'     objectPlots the posterior distribution of the range,
+#'     log(range), variance, or log(variance) parameter of a model's
+#'     SPDE component. Can also plot Matern correlation or covariance
+#'     function.
+#'
+#' @param manifold Either "R1", "S1", "R2", or "S2", from
+#'     \code{mesh$manifold}, or a full \code{inla.mesh} or \code{inla.mesh.1d} object.
+#' @param dist A vector of distances at which to calculate the
+#'     covariances/correlations
+#' @param log.range A list (mean, sd), such as produced by
+#'     inla.spde.result(...)$summary.log.range.nominal[[1]][
+#'     c("mean", "sd")]
+#' @param log.variance Either \code{NULL}, a vector of the same type
+#'     as for log.range. When \code{NULL}, the correlations are
+#'     calculated instead of the covariances.
+#' @param alpha The SPDE operator order. Default 2.
+#' @param quantile The target credible probability. Default 0.95.
+#' @param n The number of parameter combinations to use for the approximation. Default 64.
+#' @param S1.L For \code{manifold} \code{"S1"}, give the length of the cyclic interval
+#' @return A covariance or correlation (when \code{log.variance} is
+#'     \code{NULL}) function.
+#'
+#' @details
+#' Uses a Gaussian assumption for the internal model parameters, and finds a region in parameter
+#' space with approximately \code{quantile} probability.
+
+materncov.bands = function(manifold, dist, log.range,
+                           log.variance=NULL, alpha=2,
+                           quantile=0.95, n=64, S1.L=NULL) {
+    calc.cov.R <- function(dist, kappa, var) {
+        inla.matern.cov(nu=nu, kappa, x=dist, d=d, corr=TRUE) * var
+    }
+    ## Do the right thing for _nominal_ variance
+    calc.cov.S1 <- function(dist, kappa, var) {
+        L <- diff(mesh$interval)
+        dist <- dist %% L
+        out <- calc.cov.R(abs(dist), kappa, var) + calc.cov.R(abs(dist-L),
+                                                              kappa,
+                                                              var)
+        cov0 <- calc.cov.R(0, kappa, 1) ## Measure the relative contribution of each term
+        loop <- 0
+        max.loop <- 10
+        while ((cov0 > 1e-3) && (loop < max.loop)) {
+            loop <- loop+1
+            out <- out + calc.cov.R(abs(dist+L*loop), kappa, var) +
+                calc.cov.R(abs(dist-L-L*loop), kappa, var)
+            cov0 <- calc.cov.R(L*loop, kappa, 1)
+        }
+        out
+    }
+    ## Do the right thing for _nominal_ variance
+    calc.cov.S2 <- function(dist, kappa, var) {
+        inla.matern.cov.s2(nu=nu, kappa, x=dist, norm.corr=FALSE) /
+            inla.matern.cov(nu=nu, kappa, x=0, d=2, corr=FALSE) * var
+    }
+    calc.corr.R <- function(dist, kappa) {
+        inla.matern.cov(nu=nu, kappa, x=dist, d=d, corr=TRUE)
+    }
+    calc.corr.S1 <- function(dist, kappa) {
+        inla.matern.cov.s1(nu=nu, kappa, x=dist, norm.corr=FALSE) /
+            inla.matern.cov.s1(nu=nu, kappa, x=0, norm.corr=FALSE)
+    }
+    calc.corr.S2 <- function(dist, kappa) {
+        inla.matern.cov.s2(nu=nu, kappa, x=dist, norm.corr=FALSE) /
+        inla.matern.cov.s2(nu=nu, kappa, x=0, norm.corr=FALSE)
+    }
+    if (!is.character(manifold)) {
+        if (inherits(manifold, "inla.mesh") || inherits(manifold, "inla.mesh.1d")) {
+            if ((mesh$manifold == "S1") && is.null(S1.L)) {
+                S1.L <- diff(mesh$interval)
+            }
+            manifold <- manifold$manifold
+        }
+    }
+    if (manifold == "R1") {
+        d <- 1
+        calc.corr <- calc.corr.R
+        calc.cov <- calc.cov.R
+    } else if (manifold == "S1") {
+        if (is.null(S1.L)) {
+            stop(paste0("Manifold type 'S1' requires an interval length, 'S1.L'"))
+        }
+        d <- 1
+        calc.corr <- calc.corr.S1
+        calc.cov <- calc.cov.S1
+    } else if (manifold == "R2") {
+        d <- 2
+        calc.corr <- calc.corr.R
+        calc.cov <- calc.cov.R
+    } else if (manifold == "S2") {
+        d <- 2
+        calc.corr <- calc.corr.S2
+        calc.cov <- calc.cov.S2
+    } else {
+        stop(paste0("materncov.bands does not support mesh manifold type ",
+                    manifold))
+    }
+    nu <- alpha-d/2
+    if (is.null(log.variance)) {
+        qq <- qnorm(c((1-quantile)/2, 0.5, (1+quantile)/2))
+        kappas <- sqrt(8*nu)/exp(log.range$mean + log.range$sd*rev(qq))
+        out <- data.frame(lower=calc.corr(dist, kappas[1]),
+                          middle=calc.corr(dist, kappas[2]),
+                          upper=calc.corr(dist, kappas[3]))
+    } else { ## !is.null(log.variance)
+        kappa <- sqrt(8*nu)/exp(log.range$mean)
+        out <- data.frame(lower=Inf,
+                          middle=calc.cov(dist, kappa, exp(log.variance$mean)),
+                          upper=-Inf)
+        qq <- qchisq(quantile, 2)^0.5
+        log.kappas <- log(sqrt(8*nu)) - (log.range$mean + log.range$sd * c(qq,-qq))
+        log.variances <- log.variance$mean + log.variance$sd * c(-qq,qq)
+        for (angle in 2*pi*(1:n)/n) {
+            ca <- cos(angle)
+            sa <- sin(angle)
+            the.corr <- calc.cov(dist,
+                                 exp(log.kappas[1] * (1-ca)/2 +
+                                     log.kappas[2] * (ca+1)/2),
+                                 exp(log.variances[1] * (1-sa)/2+
+                                     log.variances[2] * (sa+1)/2))
+            out$lower <- pmin(out$lower, the.corr)
+            out$upper <- pmax(out$upper, the.corr)
+        }
+    }
+    out
+}
+
+
+
 #' @title Posteriors of SPDE hyper parameters and Matern correlation or covariance function.
 #' 
 #' @description
