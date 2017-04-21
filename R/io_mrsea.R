0eb28ab44281fbe78896cd6203caf23643eadf73
Fabian E. Bachl <bachlfab@gmail.com>
2017-04-21 09:25:39 +0200

    Move mrsea descriptor

--- R/io_mrsea.R
+++ R/io_mrsea.R
@@ -1,9 +1,32 @@
 #' @name mrsea
-#' @title Marine renewables strategic environmental assessment 
+#' @title Marine renewables strategic environmental assessment
 #' @docType data
-#' 
 #' @description Data imported from package MRSea, see http://creem2.st-andrews.ac.uk/software/
-#'  
+#' 
+#' @usage mrsea
+#' 
+#' @format A list of objects:
+#'  \describe{
+#'    \item{\code{points}:}{ A \code{SpatialPointsDataFrame} object containing the locations of 
+#'    XXXXX.}
+#'    \item{\code{samplers}:}{ A \code{SpatialLinesDataFrame} object containing the transect lines
+#'    that were surveyed.}
+#'    \item{\code{mesh}:}{ An \code{inla.mesh} object containing a Delaunay triangulation 
+#'    mesh (a type of discretization of continuous space) covering the survey region.}
+#'    \item{\code{boundary}:}{ An \code{SpatialPolygonsDataFrame} object defining the boundary of the 
+#'    survey region.}
+#'    \item{\code{covar}:}{ An \code{SpatialPointsDataFrame} containing sea depth estimates.}
+#'  }
+#' @source 
+#' Library \code{MRSea}.
+#' 
+#' @references 
+#' 
+#' NONE YET
+#' 
+#' @examples
+#'  data(mrsea)
+#'  ggplot() + gg(mrsea$mesh) + gg(mrsea$samplers) + gg(mrsea$points) + gg(mrsea$boundary)
 NULL
 
 # INTERNAL DATA STORAGE
