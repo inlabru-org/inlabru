b964dd72978ba579c9bf1c9f42f735b25336f232
Fabian E. Bachl <bachlfab@gmail.com>
2017-05-04 13:45:14 +0200

    Fix single object transformation

--- R/bru.spatial.R
+++ R/bru.spatial.R
@@ -66,12 +66,18 @@ spoly = function(points, crs, to.crs = NULL) {
 #' @return List of Spatial* objects
 
 stransform = function(splist, crs) {
-  for (k in 1:length(splist)) {
-    if (inherits(splist[[k]], "Spatial")) {
-      splist[[k]] = spTransform(splist[[k]], crs)
-    } else if (inherits(splist[[k]], "inla.mesh")) {
-      splist[[k]] = inla.spTransform(splist[[k]], CRSobj = crs)
+  if ( class(splist)[[1]] == "list" ) {
+    for (k in 1:length(splist)) {
+      if (inherits(splist[[k]], "Spatial")) {
+        # cn = coordnames(splist[[k]])
+        splist[[k]] = spTransform(splist[[k]], crs)
+        #coordnames(splist[[k]]) = cn
+      } else if (inherits(splist[[k]], "inla.mesh")) {
+        splist[[k]] = inla.spTransform(splist[[k]], CRSobj = crs)
+      }
     }
-  }
+  } else { 
+    splist = stransform(list(splist), crs = crs)[[1]]
+  } 
   splist
 }
