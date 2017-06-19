e0bbae3d8ba1d130e4277003451277850da8b0ff
Fabian E. Bachl <bachlfab@gmail.com>
2017-06-19 09:15:54 +0200

    Dont run example, crashes on Windows

--- R/bru.inference.R
+++ R/bru.inference.R
@@ -22,10 +22,12 @@ generate = function(...){UseMethod("generate")}
 #' 
 #' @examples
 #' 
+#' \dontrun{
 #' input.df <- data.frame(x=cos(1:10))
 #' input.df <- within(input.df, y <- 5 + 2*cos(1:10) + rnorm(10, mean=0, sd=0.1))
 #' fit.newbru <- bru(y ~ x, "gaussian", input.df)
 #' summary(fit.newbru)
+#' }
 #' 
 
 bru = function(components = y ~ Intercept,
