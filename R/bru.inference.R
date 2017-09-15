5f306c9023aeae57f0a2eaa5a4b69995f870af6a
Fabian E. Bachl <bachlfab@gmail.com>
2017-09-15 12:21:23 +0200

    Documentation improvements

--- R/bru.inference.R
+++ R/bru.inference.R
@@ -25,8 +25,8 @@ generate = function(...){UseMethod("generate")}
 #' \dontrun{
 #' input.df <- data.frame(x=cos(1:10))
 #' input.df <- within(input.df, y <- 5 + 2*cos(1:10) + rnorm(10, mean=0, sd=0.1))
-#' fit.newbru <- bru(y ~ x, "gaussian", input.df)
-#' summary(fit.newbru)
+#' fit <- bru(y ~ x, "gaussian", input.df)
+#' summary(fit)
 #' }
 #' 
 
---
+++ inst/examples/gg.inla.mesh.R
@@ -0,0 +1,31 @@
+# Load Gorilla data
+data("gorillas")
+
+# Plot mesh using default edge colors
+
+ggplot() + gg(gorillas$mesh)
+
+# Don't show interior and exterior boundaries
+
+ggplot() + gg(gorillas$mesh, interior = FALSE, exterior = FALSE)
+
+# Change the edge colors
+
+ggplot() + gg(gorillas$mesh, 
+              edge.color = "green",
+              int.color = "black",
+              ext.color = "blue"
+              )
+
+# Use the x-coordinate of the vertices to colorize the triangles
+
+xcoord = gorillas$mesh$loc[,1]
+ggplot() + 
+  gg(gorillas$mesh, color = (xcoord-580)) +
+  gg(gorillas$boundary)
+
+# Mask the colorized area by the survey boundary, i.e. only plot the inside
+
+ggplot() + 
+  gg(gorillas$mesh, color = (xcoord-580), mask = gorillas$boundary) +
+  gg(gorillas$boundary)
