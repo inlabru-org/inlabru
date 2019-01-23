9199aeb875df2137579f64cb036e88daa4d55f3b
Fabian E. Bachl <bachlfab@gmail.com>
2019-01-23 21:39:18 +0100

    Print additional CRAN check info

--- inst/misc/travis_checks.R
+++ inst/misc/travis_checks.R
@@ -1,9 +1,19 @@
-system("echo '############################## RUNNING CRAN CHECK ##############################'")
-cran_checks = do.call(c, as.list(devtools::check('/home/work', run_dont_test=FALSE, check_dir='/home')))
+system("echo '############################## RUNNING devtools::check() ##############################'")
+check_result = devtools::check('/home/work', run_dont_test=FALSE, check_dir='/home')
 
-if (file.exists("/home/inlabru.Rcheck/tests/testthat.Rout.fail")){
-  system("echo '############################## TESTTHAT FAIL LOG ##############################'")
-  system("cat /home/inlabru.Rcheck/tests/testthat.Rout.fail")
-}
+system("echo '------------------------------ results ------------------------------------------------'")
+print(str(check_result))
 
-quit('no', status=length(cran_checks))
+system("echo '------------------------------ checkdir contents --------------------------------------'")
+list.files(check_result$checkdir)
+
+system("echo '------------------------------ exit status --------------------------------------------'")
+exit_status = length(check_result$errors) + length(check_result$warnings) + length(check_result$notes)
+print(paste0("Exit status: ", exit_status))
+
+# if (file.exists("/home/inlabru.Rcheck/tests/testthat.Rout.fail")){
+#   system("echo '############################## TESTTHAT FAIL LOG ##############################'")
+#   system("cat /home/inlabru.Rcheck/tests/testthat.Rout.fail")
+# }
+
+quit('no', status=exit_status)
