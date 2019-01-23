484e2e609e806c1a7e555203b9bb7ed29d3811a3
Fabian E. Bachl <bachlfab@gmail.com>
2019-01-23 20:47:26 +0100

    Only print testthat fails if any occurred

--- inst/misc/travis_checks.R
+++ inst/misc/travis_checks.R
@@ -1,7 +1,9 @@
 system("echo '############################## RUNNING CRAN CHECK ##############################'")
 cran_checks = do.call(c, as.list(devtools::check('/home/work', run_dont_test=FALSE, check_dir='/home')))
 
-system("echo '############################## TESTTHAT LOG ##############################'")
-system("cat /home/inlabru.Rcheck/tests/testthat.Rout.fail")
+if (file.exists("/home/inlabru.Rcheck/tests/testthat.Rout.fail")){
+  system("echo '############################## TESTTHAT FAIL LOG ##############################'")
+  system("cat /home/inlabru.Rcheck/tests/testthat.Rout.fail")
+}
 
 quit('no', status=length(cran_checks))
