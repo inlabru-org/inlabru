ac57da3bf51178eae53dc3d41b34c11024109e11
Finn Lindgren <finn.lindgren@gmail.com>
2020-08-13 21:34:49 +0100

    Further test cleanup

--- tests/testthat/test_latent_fixed_effects.R
+++ tests/testthat/test_latent_fixed_effects.R
@@ -12,27 +12,34 @@ test_that("basic intercept model", {
 })
 
 test_that("basic fixed effect model", {
+  options <- list(control.inla = list(h = 0.005),
+                  num.threads = 1)
   mycomp <- y ~ 1 + x1
   mydata <- basic_fixed_effect_testdata()
   fit <- bru(mycomp,
     family = "normal",
-    data = mydata
+    data = mydata,
+    options = options
   )
 
   expect_equal(fit$summary.fixed["Intercept", ]$mean, 0.08537663)
 })
 
 test_that("basic fixed effect model, order relevance", {
+  options <- list(control.inla = list(h = 0.005),
+                  num.threads = 1)
   mydata <- basic_fixed_effect_testdata()
   mycomp1 <- y ~ Intercept + x1
   fit1 <- bru(mycomp1,
     family = "normal",
-    data = mydata
+    data = mydata,
+    options = options
   )
   mycomp2 <- y ~ x1 + Intercept
   fit2 <- bru(mycomp2,
     family = "normal",
-    data = mydata
+    data = mydata,
+    options = options
   )
 
   expect_equal(fit2$summary.fixed["Intercept", ],
