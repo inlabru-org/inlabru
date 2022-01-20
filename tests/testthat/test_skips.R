a7aa71d04fda04a6ccc5aedb5a1273e33d4bc626
Finn Lindgren <finn.lindgren@gmail.com>
2022-01-20 22:43:19 +0000

    Disable inla loading test, since it breaks the non-Suggests test

--- tests/testthat/test_skips.R
+++ tests/testthat/test_skips.R
@@ -1,5 +1,9 @@
 test_that("INLA loading", {
   skip_on_cran()
+  # Note:
   # On non-CRAN systems, INLA should be installed when running the tests
-  expect_error(local_bru_safe_inla(), NA, class = "skip")
+  # but all inla-requiring tests should be protected specifically for missing
+  # INLA.  The check below should NOT be done, as it will fail when testing
+  # without Suggests packages, which includes INLA.
+  # expect_error(local_bru_safe_inla(), NA, class = "skip")
 })
