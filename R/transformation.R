861aa91e0c44c1b2279b19246996f58ca680a2e7
Finn Lindgren <finn.lindgren@gmail.com>
2022-01-29 20:55:09 +0000

    Update package checks

--- R/transformation.R
+++ R/transformation.R
@@ -81,3 +81,46 @@ bru_inverse_transformation <- function(pfun, x, ..., tail.split. = NULL) {
   }
   res
 }
+
+
+
+
+# p = 0.5 + 0.5 * sign(q) * (1 - exp(-abs(q)))
+#   = 0.5 + 0.5 * sign(q) - 0.5 * sign(q) * exp(-abs(q))
+# 2p = 1 + sign(q) - sign(q) * exp(-abs(q))
+# 2p - sign(2p-1) -1 = -sign(2p-1) * exp(-abs(q))
+# -(2p-1-sign(2p-1))/sign(2p-1) = exp(-abs(q))
+# -(2p-1)/sign(2p-1)+1 = exp(-abs(q))
+# 1 - |2p-1| = exp(-abs(q))
+# -log(1 - |2p-1|) = abs(q)
+# -sign(2p-1) * log(1 - |2p-1|) = q
+# -sign(2p-1) * log1p(- |2p-1|) = q
+#
+# p <= 0.5: log(2p) = log(2) + log(p)
+# p >= 0.5: -log(2-2p) = log(2) + log(1-p) = q
+
+qlaplace <- function(p, lower.tail = TRUE, log.p = FALSE) {
+  q <- numeric(length(p))
+  if (lower.tail) {
+    if (log.p) {
+      upper <- p >= log(1/2)
+      q[upper] <- log(2) + p[upper]
+      q[!upper] <- -log(2) - log1p(-exp(p[!upper]))
+    } else {
+      upper <- p >= 0.5
+      q[upper] <- log(2) + log(p[upper])
+      q[!upper] <- -log(2) - log1p(-p[!upper])
+    }
+  } else {
+    if (log.p) {
+      upper <- p >= log(1/2)
+      q[upper] <- -log(2) - log1p(-exp(p[upper]))
+      q[!upper] <- log(2) + p[!upper]
+    } else {
+      upper <- p >= 0.5
+      q[upper] <- -log(2) - log1p(-p[upper])
+      q[!upper] <- log(2) + log(p[!upper])
+    }
+  }
+  q
+}
