ed0ed6003945d43a222e2af00529e9c1274560ba
Tim Meehan <tmeehan@audubon.org>
2022-08-17 16:29:55 -0600

    Update data.robins_subset.R (#152)
    * Update data.robins_subset.R
    
    * Corrected variable description syntax and ran devtools::document()
    
    * Update DESCRIPTION
    
    Co-authored-by: Finn Lindgren <Finn.Lindgren@gmail.com>

--- R/data.robins_subset.R
+++ R/data.robins_subset.R
@@ -1,21 +1,56 @@
 #' @name robins_subset
-#' @title Robins
+#' @title robins_subset
 #' @docType data
-#' @description This is the `robins_subset` dataset which is a subset of the
-#'   full Robins data set from ...
+#' @description This is the `robins_subset` dataset, which is a subset of the
+#'   full robins data set used to demonstrate a spatially varying trend coefficient
+#'   model in Meehan et al. 2019. The dataset includes American Robin counts,
+#'   along with time, location, and effort information, from Audubon Christimas Bird
+#'   Counts (CBC) conducted in six US states between 1987 and 2016.
 #'
 #' @usage data(robins_subset)
 #'
 #' @format The data are a data.frame with variables
 #'  \describe{
-#'    \item{`variablename`:}{ The description}
+#'    \item{`circle`:}{
+#'    Four-letter code of the CBC circle.
+#'    }
+#'    \item{`bcr`:}{
+#'    Numeric code for the bird conservation region encompassing the
+#'    count circle.
+#'    }
+#'    \item{`state`:}{
+#'    US state encompassing the count circle.
+#'    }
+#'    \item{`year`:}{
+#'    calendar year the count was conducted.
+#'    }
+#'    \item{`std_yr`:}{
+#'    transformed year, with 2016 = 0.
+#'    }
+#'    \item{`count`:}{
+#'    number of robins recorded.
+#'    }
+#'    \item{`log_hrs`:}{
+#'    the natural log of party hours.
+#'    }
+#'    \item{`lon`:}{
+#'    longitude of the count circle centroid.
+#'    }
+#'    \item{`lat`:}{
+#'    latitude of the count circle centroid.
+#'    }
+#'    \item{`obs`:}{
+#'    unique record identifier.
+#'    }
 #'  }
 #' @source
-#' github ...
+#' https://github.com/tmeeha/inlaSVCBC
 #'
 #'
 #' @references
-#' Citation?
+#' Meehan, T.D., Michel, N.L., and Rue, H. 2019. Spatial modeling of Audubon
+#' Christmas Bird Counts reveals fine‐scale patterns and drivers of relative
+#' abundance trends. Ecosphere, 10(4), p.e02707.
 #'
 #' @examples
 #' if (require(ggplot2, quietly = TRUE)) {
