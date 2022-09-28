d5899c2543e45050e923a471c731025b1b43bc26
@ASeatonSpatial <andy.e.seaton@gmail.com>
2022-09-28 16:39:08 +0100

    add test for gorillas sf lgcp vignette

--- tests/testthat/test_fmesher_sf.R
+++ tests/testthat/test_fmesher_sf.R
@@ -150,19 +150,51 @@ test_that("Conversion from sfc_POLYGON to inla.mesh.segment", {
 
 })
 
+test_that("sf gorillas lgcp vignette", {
+
+  # Code adapted from the lgcp vignette with an additional mesh construction
+  # step instead of using the mesh provided in the gorillas data.
+
+  # All sp objects in the example are replaced with sf equivalents.
+
+  data(gorillas, package = "inlabru")
+  gorillas_sf = list()
+  gorillas_sf$nests = sf::st_as_sf(gorillas$nests)
+  gorillas_sf$boundary = sf::st_as_sf(gorillas$boundary)
+
+  ## Build boundary information:
+  ## (fmesher supports SpatialPolygons, but this app is not (yet) intelligent enough for that.)
+  boundary <- list(
+    as.inla.mesh.segment(gorillas_sf$boundary),
+    NULL)
+
+  ## Build the mesh:
+  mesh_sf <- inla.mesh.2d(
+    boundary = boundary,
+    max.edge = c(0.24, 0.97),
+    min.angle = c(30, 21),
+    max.n = c(48000, 16000),
+    ## Safeguard against large meshes.
+    max.n.strict = c(128000, 128000),
+    ## Don't build a huge mesh!
+    cutoff = 0.01,
+    ## Filter away adjacent points.
+    offset = c(0.73, 1.55)
+  ) ## Offset for extra boundaries, if needed.
+
+  matern <- INLA::inla.spde2.pcmatern(gorillas$mesh,
+                                      prior.sigma = c(0.1, 0.01),
+                                      prior.range = c(5, 0.01))
+
+  cmp <- coordinates ~ mySmooth(main = coordinates, model = matern) + Intercept(1)
+
+  fit <- lgcp(
+    cmp,
+    data = gorillas_sf$nests,
+    samplers = gorillas_sf$boundary,
+    domain = list(coordinates = gorillas$mesh)
+  )
+
+})
 
 
-#if (FALSE) {
-#  # Testing for fmesher functions using sf objects.
-#  # For testing while editing code.  Eventually these will be the
-#  # basis for proper package tests.  For now should work
-#  # standalone by sourcing the relevant fmesher_*.R scripts directly
-#
-#  library(inlabru)
-#  library(INLA)
-#  library(sf)
-#  source(here::here("R", "fmesher_crs.R"))
-#  source(here::here("R", "sf_utils.R"))
-#  gorillas_sf <- readRDS(here::here("sf", "Data", "gorillas_sf.rds"))
-#
-#}
