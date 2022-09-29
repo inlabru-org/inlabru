af2aa52db47a4a2735034c7f98fc3ca2bda73154
Finn Lindgren <Finn.Lindgren@gmail.com>
2022-09-30 00:43:42 +0100

    bru_mapper code and documentation cleanup

--- R/effect.R
+++ R/effect.R
@@ -1176,7 +1176,8 @@ make_submapper <- function(subcomp_n,
 #' @param model A model component object
 #' @param \dots Arguments passed on to other methods
 #' @return A [bru_mapper] object defined by the model component
-#' @seealso [bru_mapper]
+#' @seealso [bru_mapper] for mapper constructor methods, and
+#' [bru_mapper_methods] for method generics and specific implementations.
 #' @export
 #' @examples
 #' if (bru_safe_inla(quietly = TRUE)) {
