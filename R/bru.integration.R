1037a666c4e1728c84b723d994b7be66cfa4e6e7
enoch26 <enochsuen26@gmail.com>
2022-10-19 14:57:24 +0100

    typo

--- R/bru.integration.R
+++ R/bru.integration.R
@@ -6,10 +6,10 @@
 #' by linear basis functions. The parameter `samplers` describes the area(s) integrated over.
 #'
 #' In case of a single dimension `samplers` is supposed to be a two-column `matrix` where
-#' each row describes the start and end point of the interval to integrate over. In the two-dimensional
+#' each row describes the start and end points of the interval to integrate over. In the two-dimensional
 #' case `samplers` can be either a `SpatialPolygon`, an `inla.mesh` or a
 #' `SpatialLinesDataFrame` describing the area to integrate over. If a `SpatialLineDataFrame`
-#' is provided it has to have a column called 'weight' in order to indicate the width of the line.
+#' is provided, it has to have a column called 'weight' in order to indicate the width of the line.
 #'
 #' The domain parameter is an `inla.mesh.1d` or `inla.mesh` object that can be employed to
 #' project the integration points to the vertices of the mesh. This reduces the final number of
@@ -596,7 +596,7 @@ cprod <- function(...) {
 # `inla.mesh.2d` object. Only those domains that are not given in the `samplers`
 # data.frame are used, plus the coordinates object, used for the spatial aspect
 # of the `samplers` object.
-# @param dnames Response name
+# @param dnames Names of dimensions
 # @param int.args List of arguments passed on to \code{ipoints}
 # @return Integration points
 
