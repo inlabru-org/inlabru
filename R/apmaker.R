f3b8b72af4992596a44a1d563f2ca53679d0e4d6
enoch26 <enochsuen26@gmail.com>
2022-10-30 16:29:05 +0000

    copy the ipmaker function to create apmaker function for aggreagted data apporach

---
+++ R/apmaker.R
@@ -0,0 +1,58 @@
+# @aliases ipoints
+# @export
+# @param samplers A `Spatial[Points/Lines/Polygons]DataFrame` object
+# @param domain A list of named integration definitions, each either a numeric
+# vector of points given integration weight 1, an `inla.mesh.1d` object, or an
+# `inla.mesh.2d` object. Only those domains that are not given in the `samplers`
+# data.frame are used, plus the coordinates object, used for the spatial aspect
+# of the `samplers` object.
+# @param dnames Names of dimensions
+# @param int.args List of arguments passed on to \code{ipoints}
+# @return Integration points
+
+
+apmaker <- function(samplers, domain, dnames,
+                    int.args = list(method = "stable", nsub = NULL)) {
+  # To allow sf geometry support, should likely change the logic to
+  # use the domain specification to determine the type of integration
+  # method to call, so that it doesn't need to rely on the domain name.
+
+  if ("coordinates" %in% dnames) {
+    spatial <- TRUE
+  } else {
+    spatial <- FALSE
+  }
+
+  # Dimensions provided via samplers (except "coordinates")
+  samp.dim <- intersect(names(samplers), dnames)
+
+  # Dimensions provided via domain but not via samplers
+  nosamp.dim <- setdiff(names(domain), c(samp.dim, "coordinates"))
+
+  # Check if a domain definition is missing
+  missing.dims <- setdiff(dnames, c(names(domain), samp.dim))
+  if (length(missing.dims > 0)) {
+    stop(paste0(
+      "Domain definitions missing for dimensions: ",
+      paste0(missing.dims, collapse = ", ")
+    ))
+  }
+  extra.dims <- setdiff(names(domain), c(samp.dim, nosamp.dim))
+  if (length(missing.dims > 0)) {
+    warning(paste0(
+      "Unexpected extra domain defintions: ",
+      paste0(extra.dims, collapse = ", ")
+    ))
+  }
+
+  if (spatial) {
+    ips <- ipoints(samplers, domain$coordinates,
+                   group = samp.dim, int.args = int.args
+    )
+  } else {
+    ips <- NULL
+  }
+
+  lips <- lapply(nosamp.dim, function(nm) ipoints(NULL, domain[[nm]], name = nm, int.args = int.args))
+  ips <- do.call(cprod, c(list(ips), lips))
+}
