96b33aeec74f781ea48435f4eec3d5d8ffb8a89d
Finn Lindgren <Finn.Lindgren@gmail.com>
2023-06-06 11:42:45 +0100

    Convergence plot updates

--- R/track_plotting.R
+++ R/track_plotting.R
@@ -137,7 +137,11 @@ make_track_plots <- function(fit) {
     pl_theme_rel +
     ggplot2::ggtitle("Rel end mode")
   pl3 <-
-    ggplot2::ggplot(track_data) +
+    ggplot2::ggplot(
+      track_data %>%
+        dplyr::filter(is.finite(.data$sd),
+                      is.finite(.data$new_linearisation))
+    ) +
     ggplot2::geom_line(ggplot2::aes(
       .data$iteration,
       .data$mode - .data$new_linearisation,
@@ -207,8 +211,58 @@ make_track_plots <- function(fit) {
     scale_y_log10() +
     ggplot2::ggtitle("|Change| / sd")
 
+  pl5b <-
+    ggplot2::ggplot(
+      track_data %>%
+        dplyr::group_by(.data$effect,
+                        .data$iteration) %>%
+        dplyr::mutate(sd = dplyr::if_else(is.finite(.data$sd), .data$sd, 1.0)) %>%
+        dplyr::summarise(
+          MaxMode = max(abs(.data$mode - .data$mode.prev) / .data$sd),
+          MeanMode = mean(abs(.data$mode - .data$mode.prev) / .data$sd),
+          RMSMode = mean((.data$mode - .data$mode.prev)^2 / .data$sd^2)^0.5,
+          MaxLin = max(abs(.data$new_linearisation - .data$new_linearisation.prev) / .data$sd),
+          MeanLin = mean(abs(.data$new_linearisation - .data$new_linearisation.prev) / .data$sd),
+          RMSLin = mean((.data$new_linearisation - .data$new_linearisation.prev)^2 / .data$sd^2)^0.5,
+          .groups = "drop") %>%
+        dplyr::mutate(
+          MaxMode = dplyr::if_else(.data$MaxMode > 0, .data$MaxMode, NA),
+          MeanMode = dplyr::if_else(.data$MeanMode > 0, .data$MeanMode, NA),
+          RMSMode = dplyr::if_else(.data$RMSMode > 0, .data$RMSMode, NA),
+          MaxLin = dplyr::if_else(.data$MaxLin > 0, .data$MaxLin, NA),
+          MeanLin = dplyr::if_else(.data$MeanLin > 0, .data$MeanLin, NA),
+          RMSLin = dplyr::if_else(.data$RMSLin > 0, .data$RMSLin, NA),
+        )
+      ) +
+    ggplot2::geom_line(ggplot2::aes(
+      .data$iteration,
+      MaxMode,
+      lty = lty_["Mode"]
+    )) +
+    ggplot2::geom_line(ggplot2::aes(
+      .data$iteration,
+      MeanMode,
+      lty = lty_["Mode"]
+    )) +
+    ggplot2::geom_line(ggplot2::aes(
+      .data$iteration,
+      MaxLin,
+      lty = lty_["Lin"]
+    )) +
+    ggplot2::geom_line(ggplot2::aes(
+      .data$iteration,
+      MeanLin,
+      lty = lty_["Lin"]
+    )) +
+    pl_theme_norm +
+    scale_y_log10() +
+    ggplot2::ggtitle("|Change| / sd (Max and Mean)")
+
   pl6 <-
-    ggplot2::ggplot(track_data) +
+    ggplot2::ggplot(
+      track_data %>%
+        dplyr::filter(is.finite(.data$sd))
+    ) +
     ggplot2::geom_line(ggplot2::aes(
       .data$iteration,
       (.data$mode - .data$mode.prev),
@@ -243,13 +297,13 @@ make_track_plots <- function(fit) {
   pl_combined <-
     ((pl1 |
       pl3 + ggplot2::guides(linetype = "none")) /
-      (pl5 + ggplot2::guides(linetype = "none") |
+      (pl5b + ggplot2::guides(linetype = "none") |
         pl6 + ggplot2::guides(linetype = "none"))) +
       patchwork::plot_layout(guides = "collect") &
       ggplot2::theme(legend.position = "bottom")
 
 
-  list(pl1, pl2, pl3, pl4, pl5, pl6,
+  list(pl1, pl2, pl3, pl4, pl5, pl5b, pl6,
     default = pl_combined
   )
 }
