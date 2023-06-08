ab39771d19b59da65a1ff03eef2551cba03db216
Finn Lindgren <Finn.Lindgren@gmail.com>
2023-06-09 00:33:45 +0100

    Add reexports documentation

--- R/fmesher.R
+++ R/fmesher.R
@@ -1,56 +1,106 @@
 #' @import fmesher
 #'
 #' @export "fm_crs_oblique<-"
+fmesher::`fm_crs_oblique<-`
 #' @export "fm_ellipsoid_radius<-"
+fmesher::`fm_ellipsoid_radius<-`
 #' @export "fm_length_unit<-"
+fmesher::`fm_length_unit<-`
 #' @export fm_CRS
+fmesher::fm_CRS
 #' @export fm_CRS_as_list
+fmesher::fm_CRS_as_list
 #' @export fm_CRSargs
+fmesher::fm_CRSargs
 #' @export fm_CRSargs_as_list
+fmesher::fm_CRSargs_as_list
 #' @export fm_as_inla_mesh
+fmesher::fm_as_inla_mesh
 #' @export fm_as_inla_mesh_segment
+fmesher::fm_as_inla_mesh_segment
 #' @export fm_as_sfc
+fmesher::fm_as_sfc
 #' @export fm_as_sp_crs
+fmesher::fm_as_sp_crs
 #' @export fm_contains
+fmesher::fm_contains
 #' @export fm_cprod
+fmesher::fm_cprod
 #' @export fm_crs
+fmesher::fm_crs
 #' @export fm_crs_get_ellipsoid_radius
+fmesher::fm_crs_get_ellipsoid_radius
 #' @export fm_crs_get_lengthunit
+fmesher::fm_crs_get_lengthunit
 #' @export fm_crs_get_wkt
+fmesher::fm_crs_get_wkt
 #' @export fm_crs_is_geocent
+fmesher::fm_crs_is_geocent
 #' @export fm_crs_oblique
+fmesher::fm_crs_oblique
 #' @export fm_crs_set_ellipsoid_radius
+fmesher::fm_crs_set_ellipsoid_radius
 #' @export fm_crs_set_lengthunit
+fmesher::fm_crs_set_lengthunit
 #' @export fm_ellipsoid_radius
+fmesher::fm_ellipsoid_radius
 #' @export fm_evaluate
+fmesher::fm_evaluate
 #' @export fm_evaluator
+fmesher::fm_evaluator
 #' @export fm_evaluator_inla_mesh
+fmesher::fm_evaluator_inla_mesh
 #' @export fm_evaluator_inla_mesh_1d
+fmesher::fm_evaluator_inla_mesh_1d
 #' @export fm_evaluator_lattice
+fmesher::fm_evaluator_lattice
 #' @export fm_has_PROJ6
+fmesher::fm_has_PROJ6
 #' @export fm_identical_CRS
+fmesher::fm_identical_CRS
 #' @export fm_int
+fmesher::fm_int
 #' @export fm_int_inla_mesh
+fmesher::fm_int_inla_mesh
 #' @export fm_int_multi_sampler
+fmesher::fm_int_multi_sampler
 #' @export fm_length_unit
+fmesher::fm_length_unit
 #' @export fm_list_as_CRS
-#' @export fm_pixels
+fmesher::fm_list_as_CRS
+## @export fm_pixels
+#fmesher::fm_pixels
 #' @export fm_proj4string
+fmesher::fm_proj4string
 #' @export fm_sp2segment
+fmesher::fm_sp2segment
 #' @export fm_spTransform
+fmesher::fm_spTransform
 #' @export fm_sp_get_crs
+fmesher::fm_sp_get_crs
 #' @export fm_transform
+fmesher::fm_transform
 #' @export fm_wkt
+fmesher::fm_wkt
 #' @export fm_wkt_as_wkt_tree
+fmesher::fm_wkt_as_wkt_tree
 #' @export fm_wkt_get_ellipsoid_radius
+fmesher::fm_wkt_get_ellipsoid_radius
 #' @export fm_wkt_get_lengthunit
+fmesher::fm_wkt_get_lengthunit
 #' @export fm_wkt_is_geocent
+fmesher::fm_wkt_is_geocent
 #' @export fm_wkt_predef
+fmesher::fm_wkt_predef
 #' @export fm_wkt_set_ellipsoid_radius
+fmesher::fm_wkt_set_ellipsoid_radius
 #' @export fm_wkt_set_lengthunit
+fmesher::fm_wkt_set_lengthunit
 #' @export fm_wkt_tree_as_wkt
+fmesher::fm_wkt_tree_as_wkt
 #' @export fm_wkt_tree_get_item
+fmesher::fm_wkt_tree_get_item
 #' @export fm_wkt_tree_set_item
+fmesher::fm_wkt_tree_set_item
 #' @export fm_wkt_unit_params
-
-NULL
+fmesher::fm_wkt_unit_params
