a0efa0052baeb19dff7786edba05a80d3587558b
Finn Lindgren <Finn.Lindgren@gmail.com>
2023-07-03 00:28:10 +0100

    Protect test against missing sn package

--- tests/testthat/test-fmesher_pixels.R
+++ tests/testthat/test-fmesher_pixels.R
@@ -1,6 +1,7 @@
 test_that("fm_pixels sp vs sf", {
   skip_on_cran()
   local_bru_safe_inla()
+  skip_if_not_installed("sn")
 
   mesh <- INLA::inla.mesh.2d(cbind(0, 0),
     offset = 10, max.edge = 1,
