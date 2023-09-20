61d2ee3ce9e85cfd3f06821c16b71b133fd49d54
Finn Lindgren <Finn.Lindgren@gmail.com>
2023-09-20 10:52:55 +0100

    Expand derivative NaN warning

--- R/nlinla.R
+++ R/nlinla.R
@@ -194,8 +194,10 @@ bru_compute_linearisation.component <- function(cmp,
       }
       nonzero <- is.finite(values)
       if (any(!nonzero)) {
-        warning("Non-finite (-Inf/Inf/NaN) entries detected in predictor derivatives; treated as 0.0",
-          immediate. = TRUE
+        warning("Non-finite (-Inf/Inf/NaN) entries detected in predictor derivatives for '",
+                state[[label]],
+                "'; treated as 0.0",
+                immediate. = TRUE
         )
       }
       nonzero[nonzero] <- (values[nonzero] != 0.0) # Detect exact (non)zeros
