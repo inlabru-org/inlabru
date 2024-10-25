d8ac190d756da076b04d296d9a3449b55100dbf0
Finn Lindgren <Finn.Lindgren@gmail.com>
2024-10-25 16:54:33 +0100

    Styler

--- R/bru.inference.R
+++ R/bru.inference.R
@@ -1820,7 +1820,8 @@ bru_index.bru <- function(object, tag = NULL, what = NULL, ...) {
       stop(paste0(
         "Invalid tag(s) '", paste(tag, collapse = ", "), "' for ",
         "bru object with tags '", paste(names(object[["bru_info"]][["lhoods"]]),
-                                        collapse = ", "), "'"
+          collapse = ", "
+        ), "'"
       ))
     }
     unlist(lapply(tag, function(x) {
