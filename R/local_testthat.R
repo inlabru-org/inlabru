4b72ab297255d9213cc16f6ed759bd2a02e06296
Finn Lindgren <Finn.Lindgren@gmail.com>
2026-04-21 14:58:48 +0100

    Use num.threads="1:1:1" for tests

--- R/local_testthat.R
+++ R/local_testthat.R
@@ -119,7 +119,7 @@ local_inla_options_set <- function(...,
 }
 
 
-#' @describeIn local_testthat Tests should set num.threads = "1:1" to ensure
+#' @describeIn local_testthat Tests should set num.threads = "1:1:1" to ensure
 #'   within-system repeatability by calling `local_bru_safe_inla()`; see also
 #'   [bru_safe_inla()]
 #' @param multicore logical; if `TRUE`, multiple cores are allowed, and the INLA
@@ -179,7 +179,7 @@ local_bru_safe_inla <- function(multicore = FALSE,
   }
 
   if (!multicore) {
-    local_bru_options_set(num.threads = "1:1", envir = envir)
+    local_bru_options_set(num.threads = "1:1:1", envir = envir)
   }
   testthat::skip_if_not(bru_safe_inla(multicore = multicore, quietly = quietly))
   # Ignore spurious warnings for INLA 23.12.17. Is fixed in later INLA versions:
