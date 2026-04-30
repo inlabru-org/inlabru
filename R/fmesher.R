bf6147d3a8e1ef8fd8f167ae95e48466a712fb8a
Finn Lindgren <Finn.Lindgren@gmail.com>
2026-04-30 13:08:35 +0100

    Typo fix

--- R/fmesher.R
+++ R/fmesher.R
@@ -1,7 +1,7 @@
 # Re-export fmesher functions for temporary backwards compatibility.
 # These functions are not intended for public use and will be removed in a
 # future release.
-# For 2.14.1, fm_int, fm_crs, and fm_pixels are kept, as they are used
+# For 2.14.1, fm_int and fm_pixels are kept, as they are used
 # by intSDM without explicitly importing from fmesher.
 
 # @export
