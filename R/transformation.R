bf4a4dc4721433023e52bc2d205e80edeb4394b2
Finn Lindgren <Finn.Lindgren@gmail.com>
2026-07-20 22:38:53 +0100

    jarl check

--- R/transformation.R
+++ R/transformation.R
@@ -101,7 +101,7 @@ bru_inverse_transformation <- function(pfun, x, ..., tail.split. = NULL) {
     }
   }
   res <- numeric(length(x))
-  if (sum(upper) > 0) {
+  if (any(upper)) {
     res[upper] <-
       qnorm(
         do.call(
@@ -119,7 +119,7 @@ bru_inverse_transformation <- function(pfun, x, ..., tail.split. = NULL) {
         log.p = TRUE
       )
   }
-  if (any(!upper)) {
+  if (!all(upper)) {
     res[!upper] <-
       qnorm(
         do.call(
